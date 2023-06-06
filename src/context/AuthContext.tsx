4858af50fd740eadb355889768c701a853ae07dd
Axely7 <axely7@hotmail.com>
2023-06-06 11:22:39 -0600

    creating authcontext, authreducer

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,7 +1,50 @@
-import {createContext} from 'react';
+import {createContext, useReducer} from 'react';
+import cafeApi from '../api/cafeApi';
+import {Usuario, LoginResponse, LoginData} from '../interfaces/appInterfaces';
+import {authReducer, AuthState} from './authReducer';
 
 type AuthContextProps = {
   errorMessage: string;
+  token: string | null;
+  user: Usuario | null;
+  status: 'checking' | 'authenticated' | 'not-authenticated';
+  signUp: () => void;
+  signIn: ({correo, password}: LoginData) => void;
+  logOut: () => void;
+  removeError: () => void;
 };
 
-const AuthContext = createContext({});
+const authInitialState: AuthState = {
+  status: 'checking',
+  token: null,
+  user: null,
+  errorMessage: '',
+};
+
+export const AuthContext = createContext({} as AuthContextProps);
+
+export const AuthProvider = ({children}: any) => {
+  const [state, dispatch] = useReducer(authReducer, authInitialState);
+
+  const signUp = () => {};
+  const signIn = async ({correo, password}: LoginData) => {
+    try {
+      const resp = await cafeApi.post<LoginResponse>('/auth/login', {
+        correo,
+        password,
+      });
+      console.log(resp.data);
+    } catch (error) {
+      console.log(error.response.data);
+    }
+  };
+  const logOut = () => {};
+  const removeError = () => {};
+
+  return (
+    <AuthContext.Provider
+      value={{...state, signUp, signIn, logOut, removeError}}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {
   Keyboard,
   KeyboardAvoidingView,
@@ -13,10 +13,13 @@ import {WhiteLogo} from '../components/WhiteLogo';
 import {loginStyles} from '../theme/loginTheme';
 import {useForm} from '../hooks/useForm';
 import {StackScreenProps} from '@react-navigation/stack';
+import {AuthContext} from '../context/AuthContext';
 
 interface Props extends StackScreenProps<any, any> {}
 
 export const LoginScreen = ({navigation}: Props) => {
+  const {signIn} = useContext(AuthContext);
+
   const {email, password, onChange} = useForm({
     email: '',
     password: '',
@@ -25,6 +28,7 @@ export const LoginScreen = ({navigation}: Props) => {
   const onLogin = () => {
     console.log({email, password});
     Keyboard.dismiss();
+    signIn({correo: email, password});
   };
 
   return (
@@ -57,7 +61,6 @@ export const LoginScreen = ({navigation}: Props) => {
           <TextInput
             placeholder="Ingrese su Contraseña"
             placeholderTextColor={'rgba(255,255,255,0.4)'}
-            keyboardType="email-address"
             underlineColorAndroid={'white'}
             style={[
               loginStyles.inputField,
